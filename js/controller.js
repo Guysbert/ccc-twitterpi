afa22edd576da514c6c3efb672a1648dfaec75f5
Andreas Houben <andreas.houben@codecentric.de>
2015-08-12 00:11:05 +0200

    angularified app

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('CCCTwitterPi').
+    controller('twitterpiController', function twitterpiController(){
+        var controller = this;
+
+        controller.twittertext="Minions ipsum bananaaaa jiji hana dul sae jiji gelatooo chasy potatoooo chasy ti aamoo! Po kass. Bananaaaa underweaaar uuuhhh hahaha para tú"
+    
+});
