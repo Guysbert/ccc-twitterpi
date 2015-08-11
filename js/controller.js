4ccdf74ca86d520bcafc37e51e0d0754d81b4da3
Andreas Houben <andreas.houben@codecentric.de>
2015-08-12 00:28:37 +0200

    added timer

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,19 @@
 'use strict';
 angular.module('CCCTwitterPi').
-    controller('twitterpiController', function twitterpiController(){
+    controller('twitterpiController', function twitterpiController($interval){
         var controller = this;
 
-        controller.twittertext="Minions ipsum bananaaaa jiji hana dul sae jiji gelatooo chasy potatoooo chasy ti aamoo! Po kass. Bananaaaa underweaaar uuuhhh hahaha para tú"
+        var text1= "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed";
+        var text2= "Minions ipsum bananaaaa jiji hana dul sae jiji gelatooo chasy potatoooo chasy ti aamoo! Po kass. Bananaaaa underweaaar uuuhhh hahaha para tú";
+        var i = 0;
+        controller.twittertext = text1;
+        $interval(function(){
+            if (controller.twittertext === text1){
+                controller.twittertext = text2;
+            } else {
+                controller.twittertext = text1;
+            }   
+        }, 2000);
+        
     
 });
